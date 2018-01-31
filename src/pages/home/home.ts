9623a71917462076b3db65e13194fd8748a0276a
Luara Mineiro <luaramineiro@gmail.com>
2018-01-31 17:01:43 -0300

    CRUD em memória [funcionando]

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,51 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AppPreferences } from '@ionic-native/app-preferences';
+import { SQLite } from '@ionic-native/sqlite';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  id_favorite:string;
-  noticias = [];
+  listNews = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public sqlite: SQLite) {
+    this.createDB();
   }
 
-  inserir(descricao) {
-    this.noticias.push({descricao});
+  getDB() {
+    return this.sqlite.create({
+      name: 'news.db',
+      location: 'default'
+    })
+  }
+
+  createDB() {
+    return this.getDB().then((sql) => {
+      sql.executeSql('CREATE TABLE news (desc TEXT)', []);
+    })
+  }
+
+  updateNews(){
+    return this.getDB().then((sql) => {
+      sql.executeSql('SELECT * FROM news;', []).then((news) => {
+        this.listNews = [];
+        for (let i = 0; i < news.rows.lenght; i++) {
+          // let new = news.rows.item(i);
+          // this.listNews.push({
+          //   desc: new.desc,
+          // });
+        }
+      });
+    });
+  }
+
+  insert(desc) {
+    this.listNews.push({desc: desc});
   }
 
   removerPrimeiro() {
-    this.noticias.shift();
+    this.listNews.shift();
   }
 }
